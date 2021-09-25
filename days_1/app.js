aea2cd53016c583951ef308fbeb18bb48104838b
hakan44-star <77295878+hakan44-star@users.noreply.github.com>
2021-09-25 15:00:25 +0200

    first commit

---
+++ days_1/app.js
@@ -0,0 +1,6 @@
+const tekCift1 = function(sayi) {
+    return sayi % 2 == 0 ? "cift" : "tek";
+
+}
+
+console.log("sayi" + " " + tekCift1(5) + " 'dir");
---
+++ dom/app.js
@@ -0,0 +1,4 @@
+const paragraf = document.getElementById("para").style;
+paragraf.backgroundColor = "black";
+paragraf.color = "white";
+paragraf.fontSize = "20px";
